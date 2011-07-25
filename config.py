e26bb27c9ba9b70dbb6bae02629ef51bcafdf507
seb26 <unleashed26@gmail.com>
2011-07-25 21:09:43 +1000

    And so it begins.

---
+++ config.py
@@ -0,0 +1,18 @@
+﻿# Copyright (c) 2011 seb26. All rights reserved.
+# Source code is licensed under the terms of the Modified BSD License.
+
+import os
+
+config = {
+
+    'username': 'unleashed26',
+    'steamdir': r'C:\Program Files\Steam'
+
+    }
+
+# Edit above this only.
+
+config['dir'] = {}
+config['dir']['steamapps'] = os.path.join(config['steamdir'], 'steamapps')
+config['dir']['common'] = os.path.join(config['steamdir'], 'steamapps', 'common')
+config['dir']['usr'] = os.path.join(config['steamdir'], 'steamapps', config['username'])
