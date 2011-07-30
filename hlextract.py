54f3eb0813166d81a7a3f3347dc75a0f0e755679
seb26 <unleashed26@gmail.com>
2011-07-30 18:33:17 +1000

    Rewriting hlextract.py with class. (H)

--- hlextract.py
+++ hlextract.py
@@ -1,75 +1,75 @@
 ﻿# Copyright (c) 2011 seb26. All rights reserved.
 # Source code is licensed under the terms of the Modified BSD License.
 
-import os, sys, shutil, re
+import os
 from collections import defaultdict
 
-import config, defn
+class HLExtract:
 
-config = config.config
-defn = defn.defn
+    def __init__(self, steamdir, username):
+        self.config = {}
+        self.config['dir'] = {}
+        self.config['dir']['steamapps'] = os.path.join(steamdir, 'steamapps')
+        self.config['dir']['common'] = os.path.join(steamdir, 'steamapps', 'common')
+        self.config['dir']['usr'] = os.path.join(steamdir, 'steamapps', username)
 
-def proper_name(s, form='proper'):
-    """Gives a proper name or abbreviated form, if given either.
-    s - the string to use.
-    form - type of name in the next param ('proper' [default] or 'abbrev').
-    [FIXME] Bug: 'left 4 dead' and 'left 4 dead 2' no like each other.
-    """
-    if form == 'proper':
-        xform = 1 # Proper name form (see defn.py)
-    elif form == 'abbrev':
-        xform = 0 # Abbreviated form
-    else:
-        pass # Pootis kul error raisin' heer.
-    result = [n for n in defn['proper_name'] if n[0] == s]
-    if len(result) == 1:
-        return result[0][xform]
-    else:
-        return result[0]
+        self.defn = {
+            'vpk': [
+                'portal2',
+                'left4dead',
+                'left4dead2',
+                'swarm'
+                ],
 
+            'proper_name': [
+                ('tf', 'team fortress 2'),
+                ('portal', 'portal'),
+                ('portal2', 'portal 2'),
+                ('left4dead', 'left 4 dead'),
+                ('left4dead2', 'left 4 dead 2'),
+                ('swarm', 'alien swarm')
+                ]
+            }
 
-def HLExtract(pkg, dpath, e, **kwargs):
-    # Process arguments heer.
-    pkg_t = pkg.split(os.extsplit)
-    if pkg_t[1] == 'vpk' and kwargs['game'] in defn['vpk']:
-        xpkg = os.path.join(config['dir']['common'], proper_name(kwargs['game']), kwargs['game'], 'pak01_dir.vpk')
-    elif pkg_t[1] == 'vpk' and pkg_t[0] != 'pak01_dir.vpk':
-        raise Exception('Error: HLExtract does not support extracting from non-directory .VPKs:', pkg_t[0])
-    else:
-        xpkg = os.path.join(config['dir']['steamapps'], pkg)
-    if 'multidir' in kwargs and kwargs['multidir'] is True:
-        if type(e) == type(list()):
+        self.hlcmd = {}
+
+    def getName(self, string, form='proper'):
+        if form == 'proper':
+            xform = 1 # Proper name form (see defn.py)
+        else:
+            xform = 0 # Abbreviated form
+        result = [n for n in self.defn['proper_name'] if n[0] == string]
+        if len(result) == 1:
+            return result[0][xform]
+        elif len(result) == 0:
+            return None
+        else:
+            return result[0]
+
+    def extract(self, package, outdir, extr, **kwargs):
+        if 'vpk' and 'game' in kwargs:
+            if kwargs['vpk'] is True and kwargs['game'] not in self.defn['vpk']:
+                raise Exception('game does not use .vpk: %s' % kwargs['game'])
+            elif kwargs['vpk'] is True:
+                tpkg = os.path.splitext(package)
+                if package == 'pak01_dir.vpk':
+                    gamename = self.getName(kwargs['game'])
+                    self.hlcmd['p'] = os.path.join(
+                        self.config['dir']['common'], gamename, kwargs['game'], 'pak01_dir.vpk')
+                elif tpkg[1] == '.gcf':
+                    raise Exception('defined package is .gcf while vpk set to true')
+                elif tpkg[1] == '.vpk' and tpkg[0] != 'pak01_dir':
+                    raise Exception('can only extract from pak01_dir.vpk, %s not supported' % package)
+        else:
+            self.hlcmd['p'] = os.path.join(self.config['dir']['steamapps'], package)
+
+        if 'multidir' in kwargs and kwargs['multidir'] is True:
             extr = {}
             mdir = defaultdict(list)
-            for fn in e:
-                wdir = os.path.split(fn)[0]
-                mdir[wdir].append(fn) # Add each fn to the appropriate working dir list.
-            for t in mdir.items():
-                fdir = t[0]
-                f = t[1]
-                extr[fdir] = '-e "' + '" -e "'.join(f) + '"'
-            print 'Run count:', len(extr.items())
-            for fdir, f in extr.items():
-                xdpath = os.path.join(dpath, pkg, fdir)
-                xdir = os.path.join(dpath, pkg)
-                if not os.path.exists(xdpath):
-                    os.makedirs(xdpath) # Make missing directories here.
-                process = os.popen(r'bin\HLExtract -p "%s" -d "%s" %s' % (xpkg, xdpath, f))
-                for i in process.readlines():
-                    print i.strip()
-                process.close()
         else:
-            raise Exception('multidir is set but extract object is not a list')
-    else:
-        if type(e) == type(list()) and len(e) > 1:
-            edir = '-e "' + '" -e "'.join(e) + '"'
-        elif type(e) == type(list()) and len(e) == 1:
-            edir = r'-e "%s"' % e
-        elif type(e) == type(str()):
-            edir = r'-e "%s"' % e
-        if not os.path.exists(dpath):
-            os.mkdir(dpath)
-        process = os.popen(r'bin\HLExtract -p "%s" -d "%s" %s' % (xpkg, dpath, edir))
-        for i in process.readlines():
-            print i.strip()
-        process.close()
+            return 'error'
+
+
+HL = HLExtract(r'C:\Program Files\Steam', 'unleashed26')
+
+print HL.extract('team fortress 2 content.gcf', r'X:\test01', r'root\tf\steam.inf', multidir=True)
