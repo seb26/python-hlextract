92f1f67ffacf608a154a70989f291a41fc32891e
seb26 <unleashed26@gmail.com>
2011-07-31 16:06:34 +1000

    * Finished cmd_console() which when given commands (e.g. 'status') returns results in a dict. * Moved cmdline options to their own function to avoid duplication.

--- hlextract.py
+++ hlextract.py
@@ -2,6 +2,7 @@
 # Source code is licensed under the terms of the Modified BSD License.
 
 import os
+import re
 from subprocess import Popen, PIPE, STDOUT
 from collections import defaultdict
 
@@ -104,29 +105,59 @@ class HLExtract:
             self.cmd_close(p, xpath, extr_cmd, options=kwargs['options'], silent=False)
 
 
-    def cmd_console(self, package, **kwargs):
-        """ EXPERIMENTAL """
+    def _cmd_options(self, options):
+            allowed = 'smqvor'
+            return '-' + ' -'.join([i for i in options if i in allowed])
+
+
+    def cmd_console(self, p, cmdl, **kwargs):
+        """ Runs a HLExtract -c process and returns results as a dict.
+        p - package name (if vpk, set to 'pak01_dir.vpk' and also set 'game' appropriately)
+        cmdl - list containing commands, e.g.: [ 'status', r'info root\tf' ]
+
+        Results are returned as a dict with the key name corresponding to the command.
+        Example:
+            x = cmd_console(package, [ 'status', r'info root\tf' ])
+            Results for 'status' and 'info root\tf' are stored in
+            x['status'] and x['info root\tf'] respectively.
+        """
         cmd_output = []
         cmd_output.append('-p "%s"' % p)
-        for z in cmdl:
-            cmd_output.append(z)
+        cmd_output.append('-c')
+        cmd_output.append('-s') # Need silent on to enable cleaner parsing.
+        cmdl_x = '-x "' + '" -x "'.join(cmdl) + '"'
+        cmd_output.append(cmdl_x)
+        cmd_output.append('-x "exit"') # Closing the process so it doesn't run forever.
         cmd = r'bin\HLExtract.exe' + ' ' + ' '.join(cmd_output)
 
-        """ Begin code that I don't fully understand yet. """
         p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
         output = []
         for z in p.stdout.readlines():
             output.append(z.strip('\r\n'))
-        return output
+        d = defaultdict(list)
+        info = {}
+        for line in output:
+            r = re.compile(r'.+?>(.+)')
+            m = r.match(line)
+            if m:
+                key = m.group(1)
+            elif line == '':
+                continue
+            else:
+                d[key].append(line.strip(' '))
+        for y in d.items():
+            x = [ i.split(': ') for i in y[1]]
+            info[y[0]] = dict((t[0].lower(), t[1]) for t in x if len(t) == 2)
+        return info
 
 
     def cmd_close(self, p, d, ev, **kwargs):
         """ Run a HLExtract process and then terminate.
-        p - package name (if vpk, set to 'pak01_dir.vpk' and set 'game' appropriately)
+        p - package name (if vpk, set to 'pak01_dir.vpk' and also set 'game' appropriately)
         d - directory to extract to
         ev - items to extract / validate (in the form: -e "path\to\file.txt" (or -t))
         kwargs: options
-            options - string, containing options to pass to HLExtract (accepts s, m, q, v, o, r (see readme))
+            options - string, additional HLExtract cmdline options.
         """
         cmd_close = []
         cmd_close.extend([ '-p "%s"' % p, '-d "%s"' % d, ev ])
@@ -135,10 +166,9 @@ class HLExtract:
             cmd_close.append('-' + kwargs['list'])
         silent = False
         if 'options' in kwargs:
-            allowed = 'smqvor' # Valid HLExtract options (see README)
-            if 's' in kwargs['options']:
+            options = self._cmd_options(kwargs['options'])
+            if 's' in options:
                 silent = True
-            cmd_close.append('-' + ' -'.join([i for i in kwargs['options'] if i in allowed]))
         cmd = r'bin\HLExtract.exe' + ' ' + ' '.join(cmd_close)
         print cmd # Debuggin'. Don't be mad.
         process = os.popen(cmd)
@@ -148,15 +178,18 @@ class HLExtract:
         process.close()
 
 
-    def cmd_output(self, p, cmdl):
+    def cmd_output(self, p, cmdl, **kwargs):
         """ Run HLExtract and return the output.
         p - package (needs full path, use getPackagePath())
-        cmdl - list, containing commands e.g.: [ '-c', '-s', '-x "cmd"' ]
+        cmdl - list, containing commands e.g.: [ '-ld', '-f' ]
+        kwargs:
+            options - string, containing options
         """
         cmd_output = []
         cmd_output.append('-p "%s"' % p)
-        for z in cmdl:
-            cmd_output.append(z)
+        if 'options' in kwargs:
+            cmd_output.append(self._cmd_options(kwargs['options']))
+        cmd_output.extend(cmdl)
         cmd = r'bin\HLExtract.exe' + ' ' + ' '.join(cmd_output)
 
         """ Begin code that I don't fully understand yet. """
