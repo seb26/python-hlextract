60fa33353285c601b173d0e6856f7cf938973b24
seb26 <unleashed26@gmail.com>
2011-08-11 13:57:52 +1000

    Move code to hllib.py, restore hlextract.py

---
+++ hllib.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2011 seb26. All rights reserved.
+# Source code is licensed under the terms of the Modified BSD License.
+
+import os
+import re
+import ctypes
+from subprocess import Popen, PIPE, STDOUT
+from collections import defaultdict
+
+class HLExtract:
+
+    def __init__(self):
+        self.dll = ctypes.WinDLL(r'.\bin\HLLib.dll')
+
+    def usedll(self):
+        return self.dll
+
+HL = HLExtract()
+print HL.usedll().CPackage()
